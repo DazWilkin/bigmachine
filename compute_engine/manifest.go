ce2cdb4f74bf050bb08deaf3fb56430c8580d083
DazWilkin <daz.wilkin@gmail.com>
2019-10-08 13:59:29 -0700

    Switched to using Container OS (COS)

---
+++ compute_engine/manifest.go
@@ -0,0 +1,47 @@
+package compute_engine
+
+import (
+	"fmt"
+
+	// TODO(dazwilkin) Probable that this would work using JSON too but YAML is used by GCE by default
+	"gopkg.in/yaml.v2"
+)
+
+type Manifest struct {
+	Spec Spec
+}
+
+// Runtime restriction that only one container is permitted
+type Spec struct {
+	Containers []Container
+}
+type Container struct {
+	Name          string   `yaml:"name"`
+	Image         string   `yaml:"image"`
+	Stdin         bool     `yaml:"stdin,omitempty"`
+	TTY           bool     `yaml:"tty,omitempty"`
+	RestartPolicy string   `yaml:"restartPolicy,omitempty"`
+	Args          []string `yaml:"args,omitempty"`
+	Env           []Env    `yaml:"env,omitempty"`
+}
+type Image struct {
+	Registry   string
+	Repository string
+	Tag        string
+}
+type Env struct {
+	Name  string `yaml:"name"`
+	Value string `yaml:"value"`
+}
+
+func (m *Manifest) String() (string, error) {
+	l := len(m.Spec.Containers)
+	if l == 0 || l > 1 {
+		return "", fmt.Errorf("[gce] Spec must contain exactly one container")
+	}
+	s, err := yaml.Marshal(&m)
+	if err != nil {
+		return "", err
+	}
+	return string(s), err
+}
---
+++ compute_engine/manifest_test.go
@@ -0,0 +1,94 @@
+package compute_engine
+
+import (
+	"testing"
+)
+
+func TestBigMachineManifest(t *testing.T) {
+	m := &Manifest{
+		Spec: Spec{
+			Containers: []Container{
+				Container{
+					Name:          "test",
+					Image:         "test",
+					Stdin:         false,
+					TTY:           false,
+					RestartPolicy: "Always",
+					Args: []string{
+						"-log=debug",
+					},
+					Env: []Env{
+						Env{
+							Name:  "BIGMACHINE_MODE",
+							Value: "machine",
+						},
+						Env{
+							Name:  "BIGMACHINE_SYSTEM",
+							Value: "gce",
+						},
+						Env{
+							Name:  "BIGMACHINE_ADDR",
+							Value: ":8443",
+						},
+					},
+				},
+			},
+		},
+	}
+	got, _ := m.String()
+
+	// Must use spaces not tabs: pay care the editor replaces spaces with tabs
+	// Must end with an newline
+	want := `spec:
+  containers:
+  - name: test
+    image: test
+    restartPolicy: Always
+    args:
+    - -log=debug
+    env:
+    - name: BIGMACHINE_MODE
+      value: machine
+    - name: BIGMACHINE_SYSTEM
+      value: gce
+    - name: BIGMACHINE_ADDR
+      value: :8443
+`
+	if got != want {
+		t.Errorf("got:\n%swant:\n%s", got, want)
+	}
+}
+func TestNotOneContainer(t *testing.T) {
+	t.Run("zero", func(t *testing.T) {
+		m := &Manifest{
+			Spec: Spec{
+				Containers: []Container{},
+			},
+		}
+		_, err := m.String()
+		if err == nil {
+			t.Errorf("Expected error: zero containers is not permitted")
+		}
+	})
+	t.Run("multiple", func(t *testing.T) {
+		m := &Manifest{
+			Spec: Spec{
+				Containers: []Container{
+					Container{
+						Name:  "name",
+						Image: "image",
+					},
+					Container{
+						Name:  "name",
+						Image: "image",
+					},
+				},
+			},
+		}
+		_, err := m.String()
+		if err == nil {
+			t.Errorf("Expected error: multiple containers is not permitted")
+		}
+
+	})
+}
